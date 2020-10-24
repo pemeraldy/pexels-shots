f49da2ac40f9bafa916661e21871764b22b86c24
pemeraldy <pemeraldy@gmail.com>
2020-10-24 21:13:13 +0100

    -

--- dist/js/chunk-2d0f00ae.06d53b54.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f00ae"],{"9b4c":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Search Screen")])])}],a={components:{}},s=a,u=t("2877"),o=Object(u["a"])(s,c,r,!1,null,"74e39353",null);n["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d0f00ae.06d53b54.js.map

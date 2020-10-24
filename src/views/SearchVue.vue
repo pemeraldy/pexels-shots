df203f685f7378d6bbe35b5e36fb813fa195c66d
pemeraldy <pemeraldy@gmail.com>
2020-10-24 07:19:09 +0100

    made a build of app...

--- src/views/SearchVue.vue
+++ src/views/SearchVue.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>
-    <h1>Search Screen</h1>
+  <div class="home pb-4">
+    <app-header />
+    <div class="d-flex justify-content-between">
+      <div class="searchNav d-flex w-100 justify-content-end">
+        <div class="btn btn-primary">Prev</div>
+        <div class="btn btn-primary">Next</div>
+        1
+      </div>
+    </div>
+    <app-gallery />
+    <app-footer />
   </div>
 </template>
+
 <script>
+// import EndPoint from "../api/api";
+import AppHeader from "@/components/AppHeader.vue";
+import AppGallery from "@/components/AppGallery.vue";
+import AppFooter from "@/components/Footer.vue";
 export default {
-  components: {},
+  components: {
+    AppHeader,
+    AppGallery,
+    AppFooter,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
 };
 </script>
 
-<style scoped></style>
+<style>
+.home {
+  background-color: #ededed;
+}
+</style>
